4dab4b0c9dc2b370725e9bac6266d756e5c088ae
TonyB <this.tonybaker@gmail.com>
2019-08-29 10:24:55 -0700

    Added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :current_user
-
+#Get the current user by session user_id
     def current_user
         @user = (User.find_by(id: session[:user_id]) || User.new)
     end
-
+#Make sure we have a user id
     def logged_in?
         current_user.id != nil
     end
-
+#Redirect if we dont have an user id
     def require_logged_in
         return redirect_to(controller 'sessions', action: 'new') unless logged_in?
     end
