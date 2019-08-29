338d5dfdb5d970393f0d65698279bdabbaae9466
TonyB <this.tonybaker@gmail.com>
2019-08-29 15:55:47 -0700

    Create routes for sessions and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: [:new, :create]
   resources :comments
   resources :rocks
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  root 'welcome#home'
 end
