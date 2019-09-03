bda2262424e2377c59ff402bd0815beafc6c822f
TonyB <this.tonybaker@gmail.com>
2019-09-03 13:03:46 -0700

    Created before action, create action, params to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,14 @@
 class CommentsController < ApplicationController
+    before_action :to_already_logged_in?
+
+    def create
+        comment = Comment.create(comment_params)
+        redirect_to comment.rock
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content, :post_id, :user_id)
+    end
 
 end
