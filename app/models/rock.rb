de644bf9310a8aba5db40712c90a753116a36a3c
TonyB <this.tonybaker@gmail.com>
2019-09-02 18:30:45 -0700

    Edited validations for rock and created edit view

--- app/models/rock.rb
+++ app/models/rock.rb
@@ -1,5 +1,5 @@
 class Rock < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  validates :name, :description, :location presence: true
+  validates :name, :description, :location, presence: true
 end
