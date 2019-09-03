90c5994dd66621c2c19cf51a9e233f1b2e05da49
TonyB <this.tonybaker@gmail.com>
2019-09-02 19:36:44 -0700

    Removed line replaced by before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,12 @@ class UsersController < ApplicationController
 #New session is checked if user is logged in, if they are sent to users/home(show). If not create new user
 #and send them on to users/new
     def new
-        #return if already_logged_in?
         @user = User.new
         render :new 
     end
 #Check if already logged in so they dont create a user while logged in. Create a new user and check if 
 #valid and saves. Set session id (log user in) and go to users/home. Redirects back to new if not.
     def create
-        #return if already_logged_in?
         @user = User.new(user_params)
         if @user.valid? && @user.save
             session[:user_id] = @user.id
