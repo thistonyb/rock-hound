584d12d77735fbe99584ca242ff7bb93099e491b
TonyB <this.tonybaker@gmail.com>
2019-09-05 19:44:33 -0700

    Added a link of member finds to welcome page,
    Deleted redundant hidden field, added password to user_attributes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :rock
 
   def user_attributes=(user_attributes)
-    self.user = User.find_or_create_by(name: user_attributes[:name]) unless user_attributes[:name].blank?
+    self.user = User.find_or_create_by(name: user_attributes[:name], password: user_attributes[:password]) unless user_attributes[:name].blank? || user_attributes[:password].blank?
   end
+
 end
