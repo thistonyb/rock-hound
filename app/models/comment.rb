5f53d64a3693d365ce5f712797af260db955311b
TonyB <this.tonybaker@gmail.com>
2019-09-10 22:03:52 -0700

    Added validates to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :rock
+  validates :content, presence: true
 
   def user_attributes=(user_attributes)
     user = User.find_by(name: user_attributes[:name])
